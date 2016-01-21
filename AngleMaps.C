a34f4e92b56c8eb4a7bf756f03508863c59124e3
Ron Belmont <belmonrj@gmail.com>
2016-01-21 13:11:41 -0500

    Updated bin number division (see code comments in previous commits) and added combined histogram and result

--- AngleMaps.C
+++ AngleMaps.C
@@ -1,7 +1,6 @@
 const float pi = TMath::Pi();
 
-//#include <map>
-//map<int,double> intphi;
+
 
 void AngleMaps(bool verbose = false)
 {
@@ -26,23 +25,23 @@ void AngleMaps(bool verbose = false)
   // --- B0
   TH2F *th2f_input_B0 = (TH2F *)file->Get("th2f_map_sensorXladder_B0_run415751");
   TH1F *th1f_ladder_B0 = (TH1F *)th2f_input_B0->ProjectionY();
-  th1f_ladder_B0->Scale(1.0/16.0); // consider getting the 16 from the TH2
+  th1f_ladder_B0->Scale(1.0/th2f_input_B0->GetNbinsX());
   TH1F *th1f_phi_B0 = new TH1F("th1f_phi_B0","",14,-3.14159,3.38241); // the ladder widths are not evenly divisible by 2pi
   // --- B1
   TH2F *th2f_input_B1 = (TH2F *)file->Get("th2f_map_sensorXladder_B1_run415751");
   TH1F *th1f_ladder_B1 = (TH1F *)th2f_input_B1->ProjectionY();
-  th1f_ladder_B1->Scale(1.0/16.0); // consider getting the 16 from the TH2
+  th1f_ladder_B1->Scale(1.0/th2f_input_B1->GetNbinsX());
   TH1F *th1f_phi_B1 = new TH1F("th1f_phi_B1","",27,-3.14159,3.26726); // the ladder widths are not evenly divisible by 2pi
   // --- B2
   TH2F *th2f_input_B2 = (TH2F *)file->Get("th2f_map_sensorXladder_B2_run415751");
   TH1F *th1f_ladder_B2 = (TH1F *)th2f_input_B2->ProjectionY();
-  th1f_ladder_B2->Scale(1.0/5.0); // consider getting the 5 from the TH2
-  TH1F *th1f_phi_B2 = new TH1F("th1f_phi_B2","",21,-3.14159,3.14159); // the ladder widths are not evenly divisible by 2pi
+  th1f_ladder_B2->Scale(1.0/th2f_input_B2->GetNbinsX());
+  TH1F *th1f_phi_B2 = new TH1F("th1f_phi_B2","",21,-3.14159,3.14159); // the ladder widths are almost evenly divisible by 2pi
   // --- B3
   TH2F *th2f_input_B3 = (TH2F *)file->Get("th2f_map_sensorXladder_B3_run415751");
   TH1F *th1f_ladder_B3 = (TH1F *)th2f_input_B3->ProjectionY();
-  th1f_ladder_B3->Scale(1.0/6.0); // consider getting the 6 from the TH2
-  TH1F *th1f_phi_B3 = new TH1F("th1f_phi_B3","",30,-3.14159,3.14159); // the ladder widths are not evenly divisible by 2pi
+  th1f_ladder_B3->Scale(1.0/th2f_input_B3->GetNbinsX());
+  TH1F *th1f_phi_B3 = new TH1F("th1f_phi_B3","",30,-3.14159,3.14159); // the ladder widths are almost evenly divisible by 2pi
 
   // --- layer, ladder
   for(int h=0; h<4; h++) // layer
@@ -57,7 +56,6 @@ void AngleMaps(bool verbose = false)
 	  float phi = geo->SensorPhiRad(h,i,0);
 	  float eff = -9;
 	  int index = -9;
-	  //intphi.insert(make_pair(index,phi));
 	  // --- B0
 	  if(B0)
 	    {
@@ -87,25 +85,51 @@ void AngleMaps(bool verbose = false)
 	    {
 	      eff = th1f_ladder_B3->GetBinContent(i+1);
 	      index = th1f_phi_B3->GetXaxis()->FindBin(phi);
-	      if(true) cout << i << " " << index << " " << phi << " " << eff << endl;
+	      if(verbose) cout << i << " " << index << " " << phi << " " << eff << endl;
 	      th1f_phi_B3->SetBinContent(index,eff);
 	    } // if B3
 	} // ladder
     } // layer
 
 
+
   th1f_phi_B0->Draw();
-  c1->Print("first_B0.png");
+  c1->Print("Figures/phi_single_B0.png");
 
   th1f_phi_B1->Draw();
-  c1->Print("first_B1.png");
+  c1->Print("Figures/phi_single_B1.png");
 
   th1f_phi_B2->Draw();
-  c1->Print("first_B2.png");
+  c1->Print("Figures/phi_single_B2.png");
 
   th1f_phi_B3->Draw();
-  c1->Print("first_B3.png");
+  c1->Print("Figures/phi_single_B3.png");
 
 
+
+  TH1F *th1f_phi_combined = new TH1F("th1f_phi_combined","",200,-pi,pi);
+
+  for(int i=0; i<200; i++)
+    {
+      float phi = th1f_phi_combined->GetBinCenter(i+1);
+      // --- B0
+      int binB0 = th1f_phi_B0->GetXaxis()->FindBin(phi);
+      float effB0 = th1f_phi_B0->GetBinContent(binB0);
+      // --- B1
+      int binB1 = th1f_phi_B1->GetXaxis()->FindBin(phi);
+      float effB1 = th1f_phi_B1->GetBinContent(binB1);
+      // --- B2
+      int binB2 = th1f_phi_B2->GetXaxis()->FindBin(phi);
+      float effB2 = th1f_phi_B2->GetBinContent(binB2);
+      // --- B3
+      int binB3 = th1f_phi_B3->GetXaxis()->FindBin(phi);
+      float effB3 = th1f_phi_B3->GetBinContent(binB3);
+      // ---
+      float effALL = effB0*effB1*effB2*effB3;
+      th1f_phi_combined->SetBinContent(i+1,effALL);
+    }
+
+  th1f_phi_combined->Draw();
+  c1->Print("Figures/phi_combined.png");
 
 }
