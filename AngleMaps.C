81997c8a1d36b275466ecdb8ec976bab61f1fefe
Ron Belmont <belmonrj@gmail.com>
2016-01-21 18:57:48 -0500

    Added eta calculations to AngleMaps

--- AngleMaps.C
+++ AngleMaps.C
@@ -25,22 +25,22 @@ void AngleMaps(bool verbose = false)
   TH2F *th2f_input_B0 = (TH2F *)file->Get("th2f_map_sensorXladder_B0_run415751");
   TH1F *th1f_ladder_B0 = (TH1F *)th2f_input_B0->ProjectionY();
   th1f_ladder_B0->Scale(1.0/th2f_input_B0->GetNbinsX());
-  TH1F *th1f_phi_B0 = new TH1F("th1f_phi_B0","",400,-pi/2,3*pi/2); // the ladder widths are not evenly divisible by 2pi
+  TH1F *th1f_phi_B0 = new TH1F("th1f_phi_B0","",400,-pi/2,3*pi/2);
   // --- B1
   TH2F *th2f_input_B1 = (TH2F *)file->Get("th2f_map_sensorXladder_B1_run415751");
   TH1F *th1f_ladder_B1 = (TH1F *)th2f_input_B1->ProjectionY();
   th1f_ladder_B1->Scale(1.0/th2f_input_B1->GetNbinsX());
-  TH1F *th1f_phi_B1 = new TH1F("th1f_phi_B1","",400,-pi/2,3*pi/2); // the ladder widths are not evenly divisible by 2pi
+  TH1F *th1f_phi_B1 = new TH1F("th1f_phi_B1","",400,-pi/2,3*pi/2);
   // --- B2
   TH2F *th2f_input_B2 = (TH2F *)file->Get("th2f_map_sensorXladder_B2_run415751");
   TH1F *th1f_ladder_B2 = (TH1F *)th2f_input_B2->ProjectionY();
   th1f_ladder_B2->Scale(1.0/th2f_input_B2->GetNbinsX());
-  TH1F *th1f_phi_B2 = new TH1F("th1f_phi_B2","",400,-pi/2,3*pi/2); // the ladder widths are almost evenly divisible by 2pi
+  TH1F *th1f_phi_B2 = new TH1F("th1f_phi_B2","",400,-pi/2,3*pi/2);
   // --- B3
   TH2F *th2f_input_B3 = (TH2F *)file->Get("th2f_map_sensorXladder_B3_run415751");
   TH1F *th1f_ladder_B3 = (TH1F *)th2f_input_B3->ProjectionY();
   th1f_ladder_B3->Scale(1.0/th2f_input_B3->GetNbinsX());
-  TH1F *th1f_phi_B3 = new TH1F("th1f_phi_B3","",400,-pi/2,3*pi/2); // the ladder widths are almost evenly divisible by 2pi
+  TH1F *th1f_phi_B3 = new TH1F("th1f_phi_B3","",400,-pi/2,3*pi/2);
 
   float phiwidth[8] = {
     0.245814, // layer 0, sublayer 0
@@ -129,7 +129,7 @@ void AngleMaps(bool verbose = false)
 
 
 
-  TH1F *th1f_phi_ALL = (TH1F *)th1f_phi_B0->Clone();
+  TH1F *th1f_phi_ALL = (TH1F *)th1f_phi_B0->Clone("th1f_phi_ALL");
   th1f_phi_ALL->Multiply(th1f_phi_B1);
   th1f_phi_ALL->Multiply(th1f_phi_B2);
   th1f_phi_ALL->Multiply(th1f_phi_B2);
@@ -152,5 +152,164 @@ void AngleMaps(bool verbose = false)
   th1f_phi_ALL->Draw("same");
 
   c1->Print("Figures/phi_ALL.png");
+
+
+
+  // -----------
+  // --- now eta
+  // -----------
+
+
+  // --- B0
+  TH1F *th1f_sensor_B0 = (TH1F *)th2f_input_B0->ProjectionX();
+  th1f_sensor_B0->Scale(1.0/th2f_input_B0->GetNbinsY());
+  TH1F *th1f_eta_B0 = new TH1F("th1f_eta_B0","",500,-2.5,2.5);
+  // --- B1
+  TH1F *th1f_sensor_B1 = (TH1F *)th2f_input_B1->ProjectionX();
+  th1f_sensor_B1->Scale(1.0/th2f_input_B1->GetNbinsY());
+  TH1F *th1f_eta_B1 = new TH1F("th1f_eta_B1","",500,-2.5,2.5);
+  // --- B2
+  TH1F *th1f_sensor_B2 = (TH1F *)th2f_input_B2->ProjectionX();
+  th1f_sensor_B2->Scale(1.0/th2f_input_B2->GetNbinsY());
+  TH1F *th1f_eta_B2 = new TH1F("th1f_eta_B2","",500,-2.5,2.5);
+  // --- B3
+  TH1F *th1f_sensor_B3 = (TH1F *)th2f_input_B3->ProjectionX();
+  th1f_sensor_B3->Scale(1.0/th2f_input_B3->GetNbinsY());
+  TH1F *th1f_eta_B3 = new TH1F("th1f_eta_B3","",500,-2.5,2.5);
+
+
+
+  float RADIUS[4] = {2.63, 5.13, 11.77, 16.69};
+  float LENGTH[4] = {22.8, 22.8, 31.8,  38.2};
+  int   NOBINS[4] = {16,16,5,6}; // 4,4,5,6 means sensors, 16,16,5,6 means chips for pixels and sensors for strips
+
+  // --- layer, sensor
+  for(int h=0; h<4; h++)
+    {
+      int nb = NOBINS[h];
+      float r = RADIUS[h];
+      float z = LENGTH[h];
+      float zmax = z/2.0;
+      float zmin = -zmax;
+      float thetamax = atan2(r,zmax);
+      float thetamin = atan2(r,zmin);
+      float etamax = -log(tan(thetamax/2.0));
+      float etamin = -log(tan(thetamin/2.0));
+      float relthetamax = thetamax - pi/2.0;
+      float relthetamin = thetamin - pi/2.0;
+      // ---
+      float div = (float)nb;
+      float zwidth = z/div;
+      float zcenter = zwidth/2.0;
+      // ---
+      for(int i=0; i<16; i++)
+	{
+	  bool B0 = (h == 0 && i < nb);
+	  bool B1 = (h == 1 && i < nb);
+	  bool B2 = (h == 2 && i < nb);
+	  bool B3 = (h == 3 && i < nb);
+	  // ---
+	  float zlo = zmin + i*zwidth;
+	  float zhi = zmin + (i+1)*zwidth;
+	  float zce = zlo + zcenter;
+	  // ---
+	  float etalo = -log(tan(atan2(r,zlo)/2.0));
+	  float etahi = -log(tan(atan2(r,zhi)/2.0));
+	  float etace = -log(tan(atan2(r,zce)/2.0));
+	  // ---
+	  float eff = -9;
+	  int indexhi = -9;
+	  int indexlo = -9;
+	  // --- B0
+	  if(B0)
+	    {
+	      eff = th1f_sensor_B0->GetBinContent(i+1);
+	      indexlo = th1f_eta_B0->GetXaxis()->FindBin(etalo);
+	      indexhi = th1f_eta_B0->GetXaxis()->FindBin(etahi);
+	      for(int j=indexlo; j<indexhi; j++) th1f_eta_B0->SetBinContent(j,eff);
+	    } // if B0
+	  // --- B1
+	  if(B1)
+	    {
+	      eff = th1f_sensor_B1->GetBinContent(i+1);
+	      indexlo = th1f_eta_B1->GetXaxis()->FindBin(etalo);
+	      indexhi = th1f_eta_B1->GetXaxis()->FindBin(etahi);
+	      for(int j=indexlo; j<indexhi; j++) th1f_eta_B1->SetBinContent(j,eff);
+	    } // if B1
+	  // --- B2
+	  if(B2)
+	    {
+	      eff = th1f_sensor_B2->GetBinContent(i+1);
+	      indexlo = th1f_eta_B2->GetXaxis()->FindBin(etalo);
+	      indexhi = th1f_eta_B2->GetXaxis()->FindBin(etahi);
+	      for(int j=indexlo; j<indexhi; j++) th1f_eta_B2->SetBinContent(j,eff);
+	    } // if B2
+	  // --- B3
+	  if(B3)
+	    {
+	      eff = th1f_sensor_B3->GetBinContent(i+1);
+	      indexlo = th1f_eta_B3->GetXaxis()->FindBin(etalo);
+	      indexhi = th1f_eta_B3->GetXaxis()->FindBin(etahi);
+	      for(int j=indexlo; j<indexhi; j++) th1f_eta_B3->SetBinContent(j,eff);
+	    } // if B3
+	} // sensor
+    } // layer
+
+
+
+  th1f_eta_B0->Draw();
+  c1->Print("Figures/eta_single_B0.png");
+
+  th1f_eta_B1->Draw();
+  c1->Print("Figures/eta_single_B1.png");
+
+  th1f_eta_B2->Draw();
+  c1->Print("Figures/eta_single_B2.png");
+
+  th1f_eta_B3->Draw();
+  c1->Print("Figures/eta_single_B3.png");
+
+
+
+  TH1F *th1f_eta_ALL = (TH1F *)th1f_eta_B0->Clone("th1f_eta_ALL");
+  th1f_eta_ALL->Multiply(th1f_eta_B1);
+  th1f_eta_ALL->Multiply(th1f_eta_B2);
+  th1f_eta_ALL->Multiply(th1f_eta_B2);
+  th1f_eta_ALL->SetLineColor(kBlack);
+  th1f_eta_ALL->Draw();
+
+  c1->Print("Figures/eta_combined.png");
+
+
+
+  th1f_eta_B0->SetLineColor(kBlue);
+  th1f_eta_B1->SetLineColor(kRed);
+  th1f_eta_B2->SetLineColor(kGreen+2);
+  th1f_eta_B3->SetLineColor(kMagenta);
+  th1f_eta_ALL->SetLineColor(kBlack);
+  th1f_eta_B0->Draw();
+  th1f_eta_B1->Draw("same");
+  th1f_eta_B2->Draw("same");
+  th1f_eta_B3->Draw("same");
+  th1f_eta_ALL->Draw("same");
+
+  c1->Print("Figures/eta_ALL.png");
+
+
+
+  TFile *file = new TFile("histograms.root","recreate");
+  file->cd();
+  th1f_phi_B0->Write();
+  th1f_phi_B1->Write();
+  th1f_phi_B2->Write();
+  th1f_phi_B3->Write();
+  th1f_phi_ALL->Write();
+  th1f_eta_B0->Write();
+  th1f_eta_B1->Write();
+  th1f_eta_B2->Write();
+  th1f_eta_B3->Write();
+  th1f_eta_ALL->Write();
+  file->Write();
+  file->Close();
 
 }
